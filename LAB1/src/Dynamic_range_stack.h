266b22ef475975c3c83c672033096e55f5508a16
AhmadAbdullah <ahmedabdullah668@gmail.com7>
2019-01-30 15:43:16 -0500

    LAB1 progress 2

--- LAB1/src/Dynamic_range_stack.h
+++ LAB1/src/Dynamic_range_stack.h
@@ -4,11 +4,11 @@
  *  - Select the current calendar term and enter the year
  *  - List students with whom you had discussions and who helped you
  *
- * uWaterloo User ID:  uwuserid @uwaterloo.ca
+ * uWaterloo User ID:  a42abdul@uwaterloo.ca
  * Submitted for ECE 250
  * Department of Electrical and Computer Engineering
  * University of Waterloo
- * Calender Term of Submission:  (Winter|Spring|Fall) 201N
+ * Calender Term of Submission:  (Winter) 2019
  *
  * By submitting this file, I affirm that
  * I am the author of all modifications to
@@ -16,7 +16,7 @@
  *
  * The following is a list of uWaterloo User IDs of those students
  * I had discussions with in preparing this project:
- *    -
+ *    -JSSAHI 
  *
  * The following is a list of uWaterloo User IDs of those students
  * who helped me with this project (describe their help; e.g., debugging):
@@ -82,53 +82,51 @@ current_capacity( initial_capacity ),
 stack_array( new int[current_capacity] ),
 maximum_array( new int[current_capacity] ),
 minimum_array( new int[current_capacity] ) {
-	// empty constructor
+
 }
 
 Dynamic_range_stack::~Dynamic_range_stack() {
-	// Enter your implementation here.
-	delete[] stack_array;	
-	delete[] maximum_array;
-	delete[] minimum_array;
+
+	clean_arrays();
 }
 
 int Dynamic_range_stack::top() const {
 
 	int top_element = stack_array[size()-1];
-	//what underflow can there be???
-	/*if( top_element == nullptr )
-		throw underflow();*/
+	
+	if( entry_count <= 0 )
+		throw underflow();
 	
 	return top_element;
 }
 
 int Dynamic_range_stack::maximum() const {
-	// Enter your implementation here.
+	
 	int maximum_element = maximum_array[size()-1];
-	//what underflow can there be??
-	/*if( maximum_element == nullptr)
-		throw underflow();*/	
+	
+	if( entry_count == 0 )
+		throw underflow();	
 
 	return maximum_element;
 }
 
 int Dynamic_range_stack::minimum() const {
-	// Enter your implementation here.
+	
 	int minimum_element = minimum_array[size()-1];
-	//what underflow can there be??
-	/*if ( minimum_element == nullptr)
-		throw underflow();*/
+	
+	if ( entry_count <= 0 )
+		throw underflow();
  
 	return minimum_element;
 }
 
 int Dynamic_range_stack::size() const {
-	// Enter your implementation here.
+	
 	return entry_count;
 }
 
 bool Dynamic_range_stack::empty() const {
-	// Enter your implementation here.
+	
 	if(size() <= 0){
 		return true;
 	}
@@ -139,7 +137,7 @@ bool Dynamic_range_stack::empty() const {
 }
 
 int Dynamic_range_stack::capacity() const {
-	// Enter your implementation here.
+	
 	return current_capacity;
 }
 
@@ -156,7 +154,7 @@ void Dynamic_range_stack::clean_arrays(){
 void Dynamic_range_stack::push( int const &obj ) {
 
 	if( (entry_count+1) > current_capacity ){
-		current_capacity = 3 * current_capacity;
+		current_capacity = 2 * current_capacity;
 
 		int *p_stack_array = new int [entry_count];
 		int *p_minimum_array = new int[entry_count];
@@ -174,13 +172,16 @@ void Dynamic_range_stack::push( int const &obj ) {
                 stack_array = new int [current_capacity];
                 minimum_array = new int[current_capacity];
                 maximum_array = new int[current_capacity];
-			
-                for( int i=0; i<entry_count; i++ ){
-                        stack_array[i] = p_stack_array[i];
-                        minimum_array[i] = p_minimum_array[i];
-                        maximum_array[i] = p_maximum_array[i];
-                }
-		 
+
+		for(int i=0; i< entry_count; i++){
+                	stack_array[i] = p_stack_array[i];
+                	minimum_array[i] = p_minimum_array[i];
+                	maximum_array[i] = p_maximum_array[i];
+		}
+
+		delete[] p_stack_array;
+		delete[] p_minimum_array;
+		delete[] p_maximum_array;
 	}
 	
 
@@ -215,24 +216,19 @@ void Dynamic_range_stack::push( int const &obj ) {
 }
 
 int Dynamic_range_stack::pop() {
-	// Enter your implementation here.
+	
+        if( entry_count <= 0 )
+                throw underflow();
+
 	int pop_element = stack_array[size()-1];
 	
-	if( entry_count <= 0 )
-		throw underflow();
-	//What other underflow can there be???
-	//how to delete the pop elements in stack??
-	/*delete &stack_array[size()-1];
-	delete &minimum_array[size()-1];
-	delete &maximum_array[size()-1];*/
-
 	entry_count--;	
 
 	return pop_element;
 }
 
 void Dynamic_range_stack::clear() {
-	// Enter your implementation here.
+	
 	max_count = 0;
 	min_count = 0;
 	entry_count = 0;
@@ -248,16 +244,12 @@ void Dynamic_range_stack::clear() {
 	}	
 }
 
-// You can modify this function however you want:  it will not be tested
 
 std::ostream &operator<<( std::ostream &out, Dynamic_range_stack const &stack ) {
-	// Print out your stacks
+	
 	for(int i = stack.size()-1; i >= 0; i--)
 		out <<"Mincount "<<stack.min_count <<" MAxcount "<<stack.max_count<<" stack value = " <<stack.stack_array[i] <<" max = "<< stack.maximum_array[i] <<" min = "<< stack.minimum_array[i]<< std::endl;
 	return out;
 }
-
-// Is an error showing up in ece250.h or elsewhere?
-// Did you forget a closing '}' ?
 
 #endif
